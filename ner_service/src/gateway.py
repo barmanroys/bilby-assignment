fdadf19f3e18a2d4c353faed68e4cb759395388a
Barman Roy, Swagato <swagatopablo@aol.com>
2025-04-26 15:17:14 +0800

    Unnecessary dependencies removed

--- ner_service/src/gateway.py
+++ ner_service/src/gateway.py
@@ -15,12 +15,19 @@ Author: Barman Roy, Swagato
 
 from typing import Sequence
 import os
-
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from uvicorn import run
+from model_interface import (
+    FrozenSet,
+    NERResult,
+    AbstractNERInterface,
+    ModelFactory,
+    config,
+    logging,
+    DEFAULT_LABELS,
+)
 
-from model_interface import FrozenSet, NERResult, AbstractNERInterface, ModelFactory, Iterator, config, logging
 
 DESCRIPTION: str = """
 A named entity recogniser application 
@@ -36,38 +43,54 @@ You will be able to POST
 and get a collection of named entities with metadata (position, label, confidence). 
 """
 
-app: FastAPI = FastAPI(title='Named Entity Recogniser',
-                       description=DESCRIPTION,
-                       contact=dict(name='Barman Roy, Swagato',
-                                    email='swagatopablo@aol.com'))
-origins = ['*']
+app: FastAPI = FastAPI(
+    title="Named Entity Recogniser",
+    description=DESCRIPTION,
+    contact=dict(name="Barman Roy, Swagato", email="swagatopablo@aol.com"),
+)
+origins = ["*"]
 # noinspection PyTypeChecker
-app.add_middleware(middleware_class=CORSMiddleware,
-                   allow_origins=origins,
-                   allow_credentials=True,
-                   allow_methods=origins,
-                   allow_headers=origins)
+app.add_middleware(
+    middleware_class=CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=origins,
+    allow_headers=origins,
+)
 
 
-
-
-
-@app.post(path='/extract_entities/')
+@app.post(path="/extract_entities/")
 async def extract(
-    text: str = Query(default='', description='Text body to extract entities from'),
-    labels: FrozenSet[str] = Query(default=frozenset(('Person', 'Company', 'Location')), description='Relevant labels for the application'),
-    threshold: float = Query(default=.5, ge=0, le=1, description='Confidence threshold to detect named entities')) -> Sequence[NERResult]:
-    """Get the recognised entities with metadata."""
-    logging.info(msg=f'Got text of length {len(text)} with labels {labels} and confidence {threshold} on the ASGI')
-    model_wrapper:AbstractNERInterface=ModelFactory().get_model_wrapper()
-    result:Iterator[NERResult]=model_wrapper.extract(text=text, labels=labels, threshold=threshold)
-    return tuple(result)
+    text: str = Query(default="", description="Text body to extract entities from"),
+    labels: FrozenSet[str] = Query(
+        default=DEFAULT_LABELS, description="Relevant labels for the application"
+    ),
+    threshold: float = Query(
+        default=0.5,
+        ge=0,
+        le=1,
+        description="Confidence threshold to detect named entities",
+    ),
+) -> Sequence[NERResult]:
+    """
+    Get the recognised entities with metadata. These are the parameters.
+    Parameters:
+            text: The text body to extract named entities from
+            labels: The labels to extract, for example, a collection of entities like person, companies etc.
+            threshold: Minimal confidence score to identify an entity. Higher value means you will get fewer results.
+    """
+    logging.info(
+        msg=f"Got text of length {len(text)} with labels {labels} and confidence {threshold} on the ASGI"
+    )
+    model_wrapper: AbstractNERInterface = ModelFactory().get_model_wrapper()
+    return tuple(model_wrapper.extract(text=text, labels=labels, threshold=threshold))
 
 
-
-if __name__ == '__main__':
-    file: str = os.path.basename(p=__file__).split(sep='.')[0]
-    appname: str = f'{file}:app'
-    host:str=config.get(section='asgi', option='HOST') # Expose the service to the network via 0.0.0.0
-    port:int=int(config.get(section='asgi', option='PORT'))
+if __name__ == "__main__":
+    file: str = os.path.basename(p=__file__).split(sep=".")[0]
+    appname: str = f"{file}:app"
+    host: str = config.get(
+        section="asgi", option="HOST"
+    )  # Expose the service to the network via 0.0.0.0
+    port: int = int(config.get(section="asgi", option="PORT"))
     run(app=appname, host=host, port=port, workers=os.cpu_count())
--- ner_service/src/model_interface.py
+++ ner_service/src/model_interface.py
@@ -2,20 +2,28 @@
 # encoding: utf-8
 
 """This file presents the abstract interface and implementation of an NER Model."""
+
 from configparser import ConfigParser
 from pydantic import BaseModel
 from abc import ABC
-from typing import Iterator, FrozenSet, Optional
+from typing import Iterator, FrozenSet
 from gliner import GLiNER
 import logging
-logging.basicConfig(format='%(asctime)s|%(levelname)s: %(message)s',
-                    datefmt='%H:%M:%S, %d-%b-%Y', level=logging.DEBUG)
+
+logging.basicConfig(
+    format="%(asctime)s|%(levelname)s: %(message)s",
+    datefmt="%H:%M:%S, %d-%b-%Y",
+    level=logging.DEBUG,
+)
 
 
 # Load basic configurations and the default hugging face path
 config: ConfigParser = ConfigParser()
-assert config.read(filenames='config.ini')
-HF_PATH: str = config.get(section='model', option='HF_PATH')
+assert config.read(filenames="config.ini")
+HF_PATH: str = config.get(section="model", option="HF_PATH")
+
+DEFAULT_LABELS: FrozenSet[str] = frozenset(("Person", "Company", "Location"))
+
 
 class NERResult(BaseModel):
     """
@@ -27,6 +35,7 @@ class NERResult(BaseModel):
         label: Deduplicated labels indicating the type of entity (e.g. PERSON, ORGANIZATION).
         score: Threshold confidence score of the recognition, ranging from 0.0 to 1.0.
     """
+
     start: int
     end: int
     text: str
@@ -37,7 +46,9 @@ class NERResult(BaseModel):
 class AbstractNERInterface(ABC):
     """Abstract interface for the NER Model."""
 
-    def extract(self, text:str, labels:FrozenSet[str], threshold:float)->Iterator[NERResult]:
+    def extract(
+        self, text: str, labels: FrozenSet[str], threshold: float
+    ) -> Iterator[NERResult]:
         """
         Extract the named entities
         Parameters:
@@ -47,37 +58,39 @@ class AbstractNERInterface(ABC):
         """
         raise NotImplementedError
 
+
 class GlinerClient(AbstractNERInterface):
     """Gliner implementation of the named entity recogniser."""
-    def __init__(self, model:GLiNER):
-        """Initialise the client with a raw model."""
-        self._model_:GLiNER=model
 
-    def extract(self, text:str,
-                labels:FrozenSet[str]=frozenset(('Person', 'Company', 'Location')),
-                threshold:float=.5) ->Iterator[NERResult]:
+    def __init__(self, model: GLiNER):
+        """Initialise the client with a raw model."""
+        self._model_: GLiNER = model
+
+    def extract(
+        self, text: str, labels: FrozenSet[str] = DEFAULT_LABELS, threshold: float = 0.5
+    ) -> Iterator[NERResult]:
         """Extract the named entities."""
-        logging.debug(msg=f'Extracting entities from {text} for {labels} and threshold {threshold}.')
-        return map(lambda entity:NERResult(**entity),
-                   self._model_.predict_entities(text=text, labels=labels, threshold=threshold))
+        logging.debug(
+            msg=f"Extracting entities from {text} for {labels} and threshold {threshold}."
+        )
+        return map(
+            lambda entity: NERResult(**entity),
+            self._model_.predict_entities(
+                text=text, labels=labels, threshold=threshold
+            ),
+        )
+
 
 class ModelFactory:
     """Model factory class for the upstream clients to get the model wrapper."""
-    def __init__(self, hf_path:str=HF_PATH):
+
+    def __init__(self, hf_path: str = HF_PATH):
         """Initialise the model for caching."""
-        self._model_:GLiNER=GLiNER.from_pretrained(pretrained_model_name_or_path=hf_path)
-        logging.info(msg=f'Downloaded model from {hf_path}')
-    def get_model_wrapper(self)->AbstractNERInterface:
+        self._model_: GLiNER = GLiNER.from_pretrained(
+            pretrained_model_name_or_path=hf_path
+        )
+        logging.info(msg=f"Downloaded model from {hf_path}")
+
+    def get_model_wrapper(self) -> AbstractNERInterface:
         """Get the NER Model client as a wrapper on the Gliner Model."""
         return GlinerClient(model=self._model_)
-
-
-
-
-
-
-
-
-
-
-
