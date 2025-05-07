9bba76ae5ae98d975f558bd8dd0a5d38573fbe9f
Barman Roy, Swagato <swagatopablo@aol.com>
2025-05-07 13:22:55 +0800

    End to end testing script validated

--- end-to-end-test.sh
+++ end-to-end-test.sh
@@ -21,9 +21,10 @@ cd ..
 minikube start --nodes=3
 kubectl apply --filename ./kubeops-manifests
 kubectl get pods --namespace bilby # This to capture the pod names for NER
-# The service can be verified by the following command and accessing loalhost:8080
+# The service can be verified by the port forward and accessing loalhost:8080
 # If the NER is using replicas, you can use any of the podnames to substitute the following variable $PODNAME
 # kubectl port-forward --namespace bilby $PODNAME 8080:8081
 
 # To trigger the cronjob manually, out of schedule for testing
 # kubectl create job --from=cronjob/ent-extraction-cronjob --namespace bilby test-job-run
+minikube dashboard
